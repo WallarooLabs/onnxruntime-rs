0e6b572f9d9ea1e5878d3ae5956fb7166be2e327
Keith Lohnes <lohnesk@gmail.com>
2022-03-04 11:01:29 -0500

    Even more clippy cleanup

--- onnxruntime/src/session.rs
+++ onnxruntime/src/session.rs
@@ -394,12 +394,8 @@ impl<'a> Session<'a> {
             .map(|n| n.into_raw() as *const i8)
             .collect();
 
-        let output_names: Vec<String> = self
-            .outputs
-            .iter()
-            .map(|output| output.name.clone())
-            .collect();
-        let output_names_cstring = output_names.into_iter().map(|n| CString::new(n).unwrap());
+        let output_names = self.outputs.iter().map(|output| output.name.clone());
+        let output_names_cstring = output_names.map(|n| CString::new(n).unwrap());
         let output_names_ptr: Vec<*const i8> = output_names_cstring
             .map(|n| n.as_ptr() as *const i8)
             .collect();
