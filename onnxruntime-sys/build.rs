3c9533b4ece841e466584a5267a8705bd67f12b0
Jason McCampbell <jasonmccampbell@protonmail.com>
2024-06-03 16:31:33 +0000

    PowerPC64 support (#20)
    * Added support for PowerPC64 architecture.
    
    Note the additional include line that may or may not be needed on other
    platforms. This is was the way the onnxruntime 1.13.1 source was
    structured.
    
    * Added PowerPC 64 bindings, corrected regex
    
    * Renamed config to config.toml to fix warning
    
    * Updated to bindgen 0.69 and regenerated

--- onnxruntime-sys/build.rs
+++ onnxruntime-sys/build.rs
@@ -85,6 +85,7 @@ fn generate_bindings(include_dir: &Path) {
                 .join("session")
                 .display()
         ),
+        format!("-I{}", include_dir.join("onnxruntime").join("core").join("providers").join("cpu").display()),
     ];
 
     // Tell cargo to invalidate the built crate whenever the wrapper changes
@@ -107,7 +108,7 @@ fn generate_bindings(include_dir: &Path) {
         .size_t_is_usize(true)
         // Format using rustfmt
         .rustfmt_bindings(true)
-        .rustified_enum("*")
+        .rustified_enum(".*")
         // Finish the builder and generate the bindings.
         .generate()
         // Unwrap the Result and panic on failure.
@@ -203,6 +204,7 @@ enum Architecture {
     X86_64,
     Arm,
     Arm64,
+    PowerPC64,
 }
 
 impl FromStr for Architecture {
@@ -214,6 +216,7 @@ impl FromStr for Architecture {
             "x86_64" => Ok(Architecture::X86_64),
             "arm" => Ok(Architecture::Arm),
             "aarch64" => Ok(Architecture::Arm64),
+            "powerpc64" => Ok(Architecture::PowerPC64),
             _ => Err(format!("Unsupported architecture: {}", s)),
         }
     }
@@ -226,6 +229,7 @@ impl OnnxPrebuiltArchive for Architecture {
             Architecture::X86_64 => Cow::from("x64"),
             Architecture::Arm => Cow::from("arm"),
             Architecture::Arm64 => Cow::from("arm64"),
+            Architecture::PowerPC64 => Cow::from("ppc64"),
         }
     }
 }
